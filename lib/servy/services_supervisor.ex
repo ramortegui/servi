d8189d03a839a05d95f7be7949cb62cb0eab9fdd
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-03-22 23:15:59 -0600

    Add Application behaviour.

--- lib/servy/services_supervisor.ex
+++ lib/servy/services_supervisor.ex
@@ -9,7 +9,7 @@ defmodule Servy.ServicesSupervisor do
   def init(:ok) do
     children = [
       Servy.PledgeServer,
-      {Servy.SensorServer, 10}
+      {Servy.SensorServer,:timer.minutes(60)}
     ]
     Supervisor.init(children, strategy: :one_for_one)
   end
